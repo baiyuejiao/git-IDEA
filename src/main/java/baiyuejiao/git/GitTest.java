b210af3de79f5caf44926b5bca35db6917e1a6d0
mj <278335581@qq.com>
2022-06-05 11:15:28 +0800

    master commit

--- src/main/java/baiyuejiao/git/GitTest.java
+++ src/main/java/baiyuejiao/git/GitTest.java
@@ -2,11 +2,10 @@ package baiyuejiao.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("Hello Master");
         System.out.println("Hello Git");
         System.out.println("Hello Git-Demo");
         System.out.println("Hello Git hot-fix");
         System.out.println("Hello Git hot-fix merge");
-
     }
 }
